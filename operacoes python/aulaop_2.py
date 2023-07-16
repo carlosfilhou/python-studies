37a8682020b98ee8ba50ab76b75411eeecec2879
Carlos dos Santos Filho <carlosfilhou@MacBook-Air-de-Carlos.local>
2023-07-16 23:13:02 +0100

    operadores relacionais

---
+++ operacoes python/aulaop_2.py
@@ -0,0 +1,29 @@
+result = 2 == 2
+
+print("o resultado da operação é igual?", result)
+print(type(result))
+
+result = 2 != 2
+
+print("o resultadpo da operação é diferente?", result)
+print(type(result))
+
+result = 2 > 2
+
+print("o resultadpo da operação é maior?", result)
+print(type(result))
+
+result = 2 < 2
+
+print("o resultadpo da operação é menor?", result)
+print(type(result))
+
+result = 2 >= 2
+
+print("o resultadpo da operação é maior ou igual?", result)
+print(type(result))
+
+result = 2 <= 2
+
+print("o resultadpo da operação é menor ou igual?", result)
+print(type(result))
