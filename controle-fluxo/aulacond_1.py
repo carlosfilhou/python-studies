867a7f6960b0a646ddfbc6b77a5098ade390f883
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-07-27 12:36:27 +0100

    nome dos arquivos e condicionais

---
+++ controle-fluxo/aulacond_1.py
@@ -0,0 +1,12 @@
+idade = input("Digite sua idade: ")
+
+idade = int(idade)
+
+if idade >= 18 and idade <= 69:
+    print("adulto")
+elif idade >= 12 and idade < 18:
+    print("adolescente")
+elif idade >= 70:
+    print("idoso")
+else:
+    print("criança")
