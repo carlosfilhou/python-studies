2d518ae0a6c5a63f83294772576850a81163c00c
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-08-10 18:46:18 +0100

    exercises file

---
+++ controle-fluxo/exer2_flux.py
@@ -0,0 +1,10 @@
+while True:
+    a = input("Digite um número quebrado ou inteiro: ")
+
+    if a == float(a) or int(a):
+        print("Seu número é: ", a)
+    else:
+        input("Seu valor é inválido, digite corretamente apenas números: ")
+
+
+# EXERCÍCIO INCOMPLETO, EM CONTRUÇÃO!
