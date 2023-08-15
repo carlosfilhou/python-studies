8ae575ab043b34c6024a3601d441331712dc836d
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-08-15 20:03:09 +0100

    docs-new-files

--- controle-fluxo/exer2_flux.py
+++ controle-fluxo/exer2_flux.py
@@ -2,25 +2,25 @@ resultado = 0
 
 while True:
     a = (input("Digite um número decimal (lembrando que se você digitar qualquer valor que não seja número o programa irá dar erro): "))
-    op = str(input("Se você quiser somar digite (+), subtrair (-), multiplicar (*) ou dividir (/): "))
+    op = str(input("Digite um operador, somar (+), subtrair (-), multiplicar (*) ou dividir (/): "))
     b = (input("Digite um número decimal (lembrando que se você digitar qualquer valor que não seja número o programa irá dar erro): "))
 
-    print(type(a)) # quero checar a classe com if para escrever para o usuário que ele não digitou um valor numérico
-
-    a = float(a)
-    b = float(b)
-
     if op == "+":
-        resultado = a + b
+            resultado = a + b
+            print("O resultado da sua operação é: ", resultado)
     elif op == "-":
-        resultado = a - b
+            resultado = a - b
+            print("O resultado da sua operação é: ", resultado)
     elif op == "*":
-        resultado = a * b
+            resultado = a * b
+            print("O resultado da sua operação é: ", resultado)
+    elif op == "/" and b == 0:
+            print("Não é possível realizar divisão por zero!")
     elif op == "/":
-        resultado = a / b
-
-    print("O resultado da sua operação é: ", resultado)
-
-
+            resultado = a / b
+            print("O resultado da sua operação é: ", resultado)
+    else:
+            print("A opção que você digitou no seu operador não é válida!")
         
-# EXERCÍCIO INCOMPLETO, EM CONTRUÇÃO!
+
+    
