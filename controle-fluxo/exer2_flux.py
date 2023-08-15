bc62d4fb871f05e340f5d6bc0ebe4e42261dc93d
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-08-15 13:11:18 +0100

    exercicio fluxo em produção

--- controle-fluxo/exer2_flux.py
+++ controle-fluxo/exer2_flux.py
@@ -1,10 +1,26 @@
+resultado = 0
+
 while True:
-    a = input("Digite um número quebrado ou inteiro: ")
+    a = (input("Digite um número decimal (lembrando que se você digitar qualquer valor que não seja número o programa irá dar erro): "))
+    op = str(input("Se você quiser somar digite (+), subtrair (-), multiplicar (*) ou dividir (/): "))
+    b = (input("Digite um número decimal (lembrando que se você digitar qualquer valor que não seja número o programa irá dar erro): "))
 
-    if a == float(a) or int(a):
-        print("Seu número é: ", a)
-    else:
-        input("Seu valor é inválido, digite corretamente apenas números: ")
+    print(type(a)) # quero checar a classe com if para escrever para o usuário que ele não digitou um valor numérico
+
+    a = float(a)
+    b = float(b)
+
+    if op == "+":
+        resultado = a + b
+    elif op == "-":
+        resultado = a - b
+    elif op == "*":
+        resultado = a * b
+    elif op == "/":
+        resultado = a / b
+
+    print("O resultado da sua operação é: ", resultado)
 
 
+        
 # EXERCÍCIO INCOMPLETO, EM CONTRUÇÃO!
