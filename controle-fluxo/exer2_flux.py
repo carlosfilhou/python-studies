74dc6ce487cec0f5c5c6dea073a3f4cbcae39887
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-08-28 10:42:42 +0100

    format

--- controle-fluxo/exer2_flux.py
+++ controle-fluxo/exer2_flux.py
@@ -1,26 +1,31 @@
 resultado = 0
 
 while True:
-    a = (input("Digite um número decimal (lembrando que se você digitar qualquer valor que não seja número o programa irá dar erro): "))
-    op = str(input("Digite um operador, somar (+), subtrair (-), multiplicar (*) ou dividir (/): "))
-    b = (input("Digite um número decimal (lembrando que se você digitar qualquer valor que não seja número o programa irá dar erro): "))
+    a = input(
+        "Digite um número decimal (lembrando que se você digitar qualquer valor que não seja número o programa irá dar erro): "
+    )
+    op = str(
+        input(
+            "Digite um operador, somar (+), subtrair (-), multiplicar (*) ou dividir (/): "
+        )
+    )
+    b = input(
+        "Digite um número decimal (lembrando que se você digitar qualquer valor que não seja número o programa irá dar erro): "
+    )
 
     if op == "+":
-            resultado = a + b
-            print("O resultado da sua operação é: ", resultado)
+        resultado = a + b
+        print("O resultado da sua operação é: ", resultado)
     elif op == "-":
-            resultado = a - b
-            print("O resultado da sua operação é: ", resultado)
+        resultado = a - b
+        print("O resultado da sua operação é: ", resultado)
     elif op == "*":
-            resultado = a * b
-            print("O resultado da sua operação é: ", resultado)
+        resultado = a * b
+        print("O resultado da sua operação é: ", resultado)
     elif op == "/" and b == 0:
-            print("Não é possível realizar divisão por zero!")
+        print("Não é possível realizar divisão por zero!")
     elif op == "/":
-            resultado = a / b
-            print("O resultado da sua operação é: ", resultado)
+        resultado = a / b
+        print("O resultado da sua operação é: ", resultado)
     else:
-            print("A opção que você digitou no seu operador não é válida!")
-        
-
-    
+        print("A opção que você digitou no seu operador não é válida!")
