3f9ca0783e73d684d3c4542f5ea191ef89eb1620
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-08-28 10:36:46 +0100

    format

--- operacoes-python/exer_op.py
+++ operacoes-python/exer_op.py
@@ -2,13 +2,14 @@ num = input("Digite um número inteiro: ")
 
 num = int(num)
 
-print("Se esse número for PAR será TRUE, e se for IMPAR será FALSE: ", num % 2 == 0) # ele está dizendo que se o resto da divisão da variável num for 0 o valor será true.
+# ele está dizendo que se o resto da divisão da variável num for 0 o valor será true.
+print("Se esse número for PAR será TRUE, e se for IMPAR será FALSE: ", num % 2 == 0)
 
 print("AQUI É O SEGUNDO EXERCICIO:")
 
 a = 5
 b = 10
-x = True 
+x = True
 y = False
 
 print((x or y) and (a < b))
