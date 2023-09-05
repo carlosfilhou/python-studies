a078d0a60576999cf4d9eb941e039f85c6ad89b2
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-09-05 23:24:04 +0100

    feat-list

--- arquitetura-dados/arq_list.py
+++ arquitetura-dados/arq_list.py
@@ -1,5 +1,33 @@
 # arquitetura de dados, listas
 
-list1 = [28, "carlos", 1.88]
+lista = [1, 10, 5, 20, 25] # criando a lista com seus respectivos indices
 
-print(list1[2])
+lista.append(3) # usando o método "append" para adicionar um valor ao final da lista
+
+print("Aqui está a lista \n", lista)
+
+lista.insert(0 ,0) # usando o método insert para inserir um valor no índice desejado
+
+print("Aqui está a lista com um valor inserido no primeiro índice \n", lista)
+
+lista.sort() # usando o método "sort" para odernar os valores entre sí
+
+print("Aqui está a lista de forma ordenada \n", lista)
+
+lista.sort(reverse=True) # usando o reverse para colocar a lista de forma decrescente
+
+print("Aqui está a lista de forma decrescente \n", lista)
+
+x = lista.pop() # usando o método "pop" para remover o último valor da lista e adicionando ele em uma variável (pode escolher o índice)
+
+print("Aqui está a lista com o último valor removido \n", lista)
+
+print("Aqui está o último valor que foi removido \n", x)
+
+# aqui um exemplo de que no Python você pode armazenar valores de qualquer tipo em uma lista
+
+cadastro = ["Carlos", 28, 1.88] # lembrando que o NOME está na posição/índice 0, a idade está no 1 e assim por diante...
+
+print("Imprimindo a lista cadastro \n", cadastro)
+
+print("Imprimindo a altura da pessoa cadastrada \n", cadastro[2])
