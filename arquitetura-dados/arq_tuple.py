5504b4e5c46bcc01b03b63b9e48cb089486ec5ae
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-09-14 11:42:09 +0100

    feat-tuples

---
+++ arquitetura-dados/arq_tuple.py
@@ -0,0 +1,5 @@
+# tuplas são imutáveis, ou seja, você não pode depois de ter criado uma 'tupla', adicionar, remover ou alterar os seus elementos
+
+x = (1, 50, 20) # tuplas podem ser criadas com parenteses
+z = 1, 50, 20 # ou sem parenteses
+
