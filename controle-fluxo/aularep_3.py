0431559d16f9f61e1332a7a72d79dcb4640fa764
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2023-10-03 21:25:35 +0100

    doc-names

--- controle-fluxo/aularep_3.py
+++ controle-fluxo/aularep_3.py
@@ -1,7 +1,7 @@
 total = 0
 
 while True:
-    valor = float(input("Digite o valor da compra: \n")) # aqui estamos "envelopando" o input para que ele seja do tipo float
+    valor = float(input("Digite o valor da compra aqui: \n")) # aqui estamos "envelopando" o input para que ele seja do tipo float
     total = total + valor
 
     continuar = input("Deseja continuar? (s/n)")
