d94cdf890e4ab559e5aee687567eadce1da22ddc
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2024-12-17 06:43:25 +0000

    add op-relacionais

---
+++ op_relacionais.py
@@ -0,0 +1,21 @@
+# OPERADORES RELACIONAIS
+
+# (RETORNAM VALOR BOOLEANOS)
+
+# COMPARAÇÃO ==
+resultado = 2 == 2  # 2 é igual a 2? (true)
+
+# DIFERENÇA !=
+resultado = 2 != 2  # 2 é diferente de 2? (false)
+
+# MAIOR
+resultado = 2 > 2  # 2 é maior que 2? (false)
+
+# MENOR
+resultado = 2 < 2  # 2 é menor que 2? (false)
+
+# MAIOR OU IGUAL
+resultado = 2 >= 2  # 2 é maior ou igual a 2? (true)
+
+# MENOR OU IGUAL
+resultado = 2 <= 2  # 2 é menor ou igual a 2? (true)
