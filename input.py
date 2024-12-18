3ff0b978a88123f2c0f6f7022b9e0c8a0e23c196
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2024-12-18 07:40:25 +0000

    add input-file

---
+++ input.py
@@ -0,0 +1,16 @@
+# INPUT
+
+# REBENDO INPUT DO USUARIO
+idade = input("Digite a idade: ")
+# idade automaticamente será uma STRING
+print("sua idade é: ", idade, " (esse valor é uma STRING)")
+
+# EM PYTHON O VALOR RETORNADO NO INPUT SEMPRE SERÁ UMA STRING
+# PARA ALTERAR, SERÁ NECESSÁRIO USAR O MÉTODO (INT)
+dias = input("Digite a quantidade de dias: ")
+dias = int(dias)  # usando método (int), também podemos usar o float)
+print("o número de dias é: ", dias, " (esse valor é do tipo INT)")
+
+
+# PARA INSERIR UMA QUEBRA DE LINHA É USADO O CARACTER ESPECIAL \n
+print("sua idade está uma linha abaixo \n", idade)
