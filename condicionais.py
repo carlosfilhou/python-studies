aa17985be6262296b0cc1c9ff757401ef95fb086
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2024-12-20 11:15:44 +0000

    add cond

---
+++ condicionais.py
@@ -0,0 +1,21 @@
+# CONDICIONAIS
+
+idade = input("Digite a sua idade: ")
+idade = int(idade)
+
+if idade >= 18:
+    print("Você é adulto")
+elif idade >= 12:  # elif (else if)
+    print("Você é adolescente")
+else:
+    print("Você é criança")
+
+
+# Lembrando que a condição não precisa ser um valor booleano,
+# o Python irá analisar a espressão como se fosse um booleano.
+# Então na verdade o interpretador vai aplicar um bool() em volta
+# de qualquer expressão que estiver na condicional.
+
+# TRUTHY e FALSY
+
+# 3 VALORES CONSIDERADOS FALSY: 0, " ", None. O restante em sua maioria TRUTHY
