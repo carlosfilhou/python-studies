28fcd1f4e5a9a189c582653d20b17c9f20ff09cf
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2025-01-04 13:48:52 +0000

    add funcoes-keyword-arguments

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,26 @@
 # DEF (função para DEFINIR funções)
 # É MAIS USADO PARA CRIAR UM BLOCO DE CÓDIGO REUTILIZÁVEL QUE PODE SER CHAMADO VÁRIAS VEZES.
 
+# definindo uma função antes de executa-la
+def dar_boas_vindas():
+    print("Olá")
+    print("Mundo!")
+
+
+# executar uma função previamente definida
+dar_boas_vindas()
+
+
+# -----------------------------------------
+
+# função com parâmetro
+def bem_vindo_ao_time(nome, sobrenome, cidade):  # PARÂMETRO
+    print("Olá,", nome, sobrenome)
+    print("Bem-vindo a", cidade)
+
+
+# ARGUMENTOS POSICIONAIS: ATENÇÃO NA SEQUÊNCIA OU POSIÇÃO DOS ARGUMENTOS!
+bem_vindo_ao_time("Carlos", "Augusto", "Dublin")  # ARGUMENTO
+
+# ARGUMENTOS NOMEADOS (keyword arguments):
+bem_vindo_ao_time(sobrenome="Paulo", nome="João", cidade="Florianópolis")
