f7b9c07f35c0488ed157a3d600347b235b66ad74
Carlos Augusto dos Santos Filho <carlosfilho.canal@icloud.com>
2025-01-04 15:37:32 +0000

    add funcoes-return

--- funcoes.py
+++ funcoes.py
@@ -1,10 +1,12 @@
 # DEF (função para DEFINIR funções)
 # É MAIS USADO PARA CRIAR UM BLOCO DE CÓDIGO REUTILIZÁVEL QUE PODE SER CHAMADO VÁRIAS VEZES.
+# ARMAZENA UMA LÓGICA OU REGRA DE NEGÓCIO
 
 # definindo uma função antes de executa-la
 def dar_boas_vindas():
     print("Olá")
     print("Mundo!")
+    # por baixo dos panos toda função tem um "return" que retorna um none
 
 
 # executar uma função previamente definida
@@ -13,7 +15,8 @@ dar_boas_vindas()
 
 # -----------------------------------------
 
-# função com parâmetro
+
+# definindo uma função com parâmetro
 def bem_vindo_ao_time(nome, sobrenome, cidade):  # PARÂMETRO
     print("Olá,", nome, sobrenome)
     print("Bem-vindo a", cidade)
@@ -24,3 +27,24 @@ bem_vindo_ao_time("Carlos", "Augusto", "Dublin")  # ARGUMENTO
 
 # ARGUMENTOS NOMEADOS (keyword arguments):
 bem_vindo_ao_time(sobrenome="Paulo", nome="João", cidade="Florianópolis")
+
+
+# ----------------------------------------
+
+
+# definindo uma função que retorna
+def calcular_conta(consumo, taxa_servico):
+    # early return ou retorno antecipado
+    if taxa_servico == 0:
+        return consumo
+
+    servico = consumo * taxa_servico
+    valor = consumo + servico
+    return valor
+
+
+# variável recebendo uma função que retorna algo
+valor_final = calcular_conta(consumo=100, taxa_servico=0.1)
+
+# printando a variável
+print("O valor calculado é:", valor_final)
